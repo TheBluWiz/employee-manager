cc9169dce91407d164df58fd62234a48ebd7ecdc
Jamey Wicklund <theBluWiz@icloud.com>
2022-12-17 22:01:03 -0800

    Switches db queries to promises

--- index.js
+++ index.js
@@ -1,96 +1,168 @@
 const inquirer = require("inquirer");
-const mysql = require("mysql2");
+const mysql = require('mysql2');
 const cTable = require('console.table');
 
 const db = mysql.createConnection(
-    {
-        host: 'localhost',
-        // MySQL username,
-        user: 'root',
-        // MySQL password
-        password: '',
-        database: 'employee_list_db'
-    },
-    console.log(`Connected to the employee_list_db.`)
+  {
+    host: 'localhost',
+    // MySQL username,
+    user: 'root',
+    // MySQL password
+    password: '',
+    database: 'employee_list_db'
+  },
 );
 
 const viewDepartments = function () {
-    db.query('SELECT name as Deparment, id as ID FROM departments;', function (err, res) {
-        err ? console.log(err) : console.table(res)
-    });
+  db.promise().query('SELECT name as Department, id as ID FROM departments;')
+    .then(([rows, fields]) => {
+      console.table(rows);
+      actionChoice();
+    })
+    .catch(() => {
+      console.log(`Here is the Error:
+      ${err}`);
+      actionChoice();
+    })
 }
 
 const viewRoles = function () {
-    db.query('SELECT title as Title, roles.id as ID, departments.name as Department, salary as Salary FROM roles JOIN departments ON roles.department_id = departments.id;', function (err, res) {
-        err ? console.log(err) : console.table(res)
-    });
+  db.promise().query('SELECT title as Title, roles.id as ID, departments.name as Department, salary as Salary FROM roles JOIN departments ON roles.department_id = departments.id;')
+    .then(function ([rows, fields]) {
+      console.table(rows)
+      actionChoice();
+    })
+    .catch(() => {
+      console.log(err)
+      actionChoice();
+    })
 }
 
 const viewEmployees = function () {
-    db.query('SELECT employees.id as ID, first_name as FirstName, last_name as LastName, roles.title as Role, departments.name as Department, roles.salary as Salary, employees.manager_id as Manager  FROM employees JOIN roles ON employees.role_id = roles.id JOIN departments ON roles.department_id = departments.id;', function (err, res) {
-        err ? console.log(err) : console.table(res)
-    });
+  db.promise().query('SELECT employees.id as ID, first_name as FirstName, last_name as LastName, roles.title as Role, departments.name as Department, roles.salary as Salary, employees.manager_id as Manager  FROM employees JOIN roles ON employees.role_id = roles.id JOIN departments ON roles.department_id = departments.id;')
+    .then(function ([rows, fields]) {
+      console.table(rows)
+      actionChoice();
+    })
+    .catch(() => {
+      console.log(err)
+      actionChoice();
+    })
 }
 
-const actionArray = [
+const addDepartment = function () {
+  inquirer.prompt([
     {
-        type: 'list',
-        message: 'What would you like to do?',
-        choices: [
-            'View All Employees',
-            'Add Employee',
-            'Update Employee Role',
-            'View All Roles',
-            'Add Role',
-            'View All Departments',
-            'Add Department',
-            'Quit'],
-        name: "action"
+      type: "prompt",
+      message: "What is the name of the department? ",
+      name: "newDepartment"
     }
-]
+  ]).then((res) => {
+    db.promise().query(`INSERT INTO departments (name) VALUES (?);`, res.newDepartment)
+  }).then(function (err, res) {
+    if (err) {
+      console.log(err)
+      actionChoice();
+    }
+    else {
+      console.log("Department Added!")
+      actionChoice();
+    }
+  })
+}
+
+const addRole = function () {
+  let currentDepartmentsArray = [];
+  let currentDepartmentsObj = {};
+  db.promise().query('SELECT name as Department, id as ID FROM departments;')
+    .then(function ([rows, fields]) {
+      currentDepartmentsObj = rows;
+      currentDepartmentsArray = [];
+      let department_id;
+      rows.map(function (element) {
+        currentDepartmentsArray.push(element.Department)
+      })
+      console.log(`These Departments are available as questions ${currentDepartmentsArray}`);
+      inquirer.prompt([
+        {
+          type: "prompt",
+          message: "What is the name of the role? ",
+          name: "title"
+        },
+        {
+          type: "prompt",
+          message: "What is the salary of the role? ",
+          name: "salary"
+        },
+        {
+          type: "list",
+          message: "Which department does the role belong to?",
+          choices: currentDepartmentsArray,
+          name: "department_id"
+        },
+      ]).then((res) => {
+        currentDepartmentsObj.forEach(element => {
+          if (element.Department === res.department_id) department_id = element.ID;
+        });
+        db.promise().query(`INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)`, [res.title, res.salary, department_id])
+          .then(function () {
+            console.log("Role Added");
+            actionChoice();
+          })
+          .catch(() => {
+            console.log(err)
+            actionChoice();
+          })
+      })
+    })
+}
 
 const actionChoice = function () {
-    inquirer.prompt(actionArray).then((response) => {
-        switch (response.action) {
-            case 'View All Employees':
-                viewEmployees();
-                actionChoice();
-                break;
-            case 'Add Employee':
-                // addEmployee();
-                console.log("Added Employee");
-                actionChoice();
-                break;
-            case 'Update Employee Role':
-                // updateRole();
-                console.log("Updated Employee Role");
-                actionChoice();
-                break;
-            case 'View All Roles':
-                viewRoles();
-                console.log("Viewed Roles");
-                actionChoice();
-                break;
-            case 'Add Role':
-                // addRole();
-                console.log("Added Role");
-                actionChoice();
-                break;
-            case 'View All Departments':
-                viewDepartments();
-                actionChoice();
-                break;
-            case 'Add Department':
-                // addDepartment();
-                console.log("Added Department");
-                actionChoice();
-                break;
-            case 'Quit':
-                db.end(console.log("Exiting Application"))
-                break;
-        }
-
-    })
+  inquirer.prompt([
+    {
+      type: 'list',
+      message: 'What would you like to do?',
+      choices: [
+        'View All Departments',
+        'View All Roles',
+        'View All Employees',
+        'Add Employee',
+        'Update Employee Role',
+        'Add Role',
+        'Add Department',
+        'Quit'],
+      name: "action"
+    }
+  ]).then((response) => {
+    switch (response.action) {
+      case 'View All Departments':
+        viewDepartments();
+        break;
+      case 'View All Roles':
+        viewRoles();
+        break;
+      case 'View All Employees':
+        viewEmployees();
+        break;
+      case 'Add Department':
+        addDepartment();
+        break;
+      case 'Add Role':
+        addRole();
+        break;
+      case 'Add Employee':
+        // addEmployee();
+        console.log("Added Employee");
+        break;
+      case 'Update Employee Role':
+        // updateRole();
+        console.log("Updated Employee Role");
+        break;
+      case 'Quit':
+        db.end(console.log("Exiting Application"))
+        break;
+    }
+  })
 }
 
 actionChoice();
